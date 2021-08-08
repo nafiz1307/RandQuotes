99c0ee8b0ae088fee932aaea82d15743b6fbebf8
nafiz1307 <55802844+nafiz1307@users.noreply.github.com>
2021-08-08 18:36:48 +0600

    Update sript.js
    implemented random generation of quotes

--- sript.js
+++ sript.js
@@ -1,11 +1,27 @@
+const quoteContainer = document.getElementById('quote-container')
+
+
+
+
 let apiQuotes =[];
 //Show New Quote
+const newQuotes=()=>{
+    //Picking Random Quote
+    const quote = apiQuotes[Math.floor(Math.random()*apiQuotes.length)];
+    console.log(quote)
+}
+
+
+
+
+//Get Quote
 const getQuotes= async()=>{
     const apiUrl = 'https://type.fit/api/quotes';
     try{
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        console.log(apiQuotes[13]);
+        // console.log(apiQuotes[13]);
+        newQuotes()
     }catch(error){
         console.log(error)
     }
